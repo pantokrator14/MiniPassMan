f946e1b3af2444973480b0c1f8d52e969caa8e47
Julio <juliusjosepham@protonmail.com>
2022-03-09 11:11:40 -0400

    creating menu and cleaning functions

---
+++ src/func/princ.py
@@ -0,0 +1,35 @@
+import imp
+from os import system, name 
+from time import sleep
+
+#nos permite limpiar la terminal al iniciar cada pantalla
+def clear():
+    #windows
+    if name == 'nt':
+        _ = system('cls') #Realmente podria nombrarla como sea, pero como por defecto python usa el piso por defecto, pues...
+    #UNIX
+    else:
+        _ = system('clear')
+
+
+#Menu inicial del sistema
+def menu():
+    clear() #Primero limpiamos
+
+    print('¿Qué quieres hacer?')
+    print('1. Guardar nueva contraseña.')
+    print('2. Mostrar contraseñas guardadas.')
+    print('3. Modificar contraseña')
+    print('4. Borrar contraseña.')
+    print('5. Generar contraseña aleatoriamente')
+    print('6. Salir')
+    opcion = input('Elige tu opción: ')
+
+    #Verificamos que se haya escogido la opción correcta
+    if opcion == 0:
+        clear()
+        print('Escoge una opción valida... -.-')
+        sleep(3)
+        menu()
+    else:
+        return opcion #Si todo está bien, permite usar la opcion para escoger la siguiente pantalla
