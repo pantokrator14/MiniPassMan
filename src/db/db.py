b93ed441a4c4d7ca1ffdfc1e93479069335a6f66
Julio <juliusjosepham@protonmail.com>
2022-03-11 12:59:48 -0400

    new basic functions

--- src/db/db.py
+++ src/db/db.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from ssl import _PasswordType
 import pymongo as pym
 
@@ -14,13 +15,28 @@ collection = db['passwords']
 def crear():    #Crear contraseñas
     opcion = input("1.Colocar mi propia contraseña \n2.Generar contraseña aleatoria automáticamente")
     if opcion == 1:
+        sitio = input("Nombre del sitio web: ")
         usuario = input("Ingrese nombre de usuario: ")
         contraseña = input("Ingrese la contraseña: ")
         nota = input("Ingrese una nota o descripción: ")
-        collection.insert_one({"username":usuario, "password":contraseña, "descripcion":nota})
+        collection.insert_one({"website":sitio, "username":usuario, "password":contraseña, "descripcion":nota})
     elif opcion == 2:
         print("Espere mientras creamos su contraseña...")
     else:
         print("ingrese una opción valida... -.-")
         crear()
+
+def buscar():
+    nombre = input("¿Como se llama el sitio web que busca? ")
+    resultado = collection.find({"website": nombre})
+    if resultado == NULL:
+        print("Sitio web no encontrado... Intente nuevamente.")
+        buscar()
+    else:
+        for res in resultado:
+            print(res['username', 'password', 'descripcion'])
+
+def borrar():
+    nombre = input("¿Como se llama el sitio web que busca? ")
+    resultado = collection.delete_one({"website": nombre})
 
